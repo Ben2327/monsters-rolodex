636d2f92c5b9c98daa4226752ac9d3930ab9ce16
Benjamin Abraham <benjaminab1184@gmail.com>
2020-04-24 13:06:29 +0530

    Updates

---
+++ precache-manifest.ac34a65062ba288d53c50aadb4d1252a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d76d228a93e33f7328ea27fec8596a9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2e33603d6655a0e35602",
+    "url": "/monsters-rolodex/static/css/main.a6067ec1.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e33603d6655a0e35602",
+    "url": "/monsters-rolodex/static/js/main.7ad59eea.chunk.js"
+  },
+  {
+    "revision": "46fcc5578023f1586c35",
+    "url": "/monsters-rolodex/static/js/runtime-main.7affd505.js"
+  }
+]);
